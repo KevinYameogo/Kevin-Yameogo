0897804d4068399cc90728645dac3a4843a5651d
KevinYameogo <yameokevin234@gmail.com>
2025-09-27 17:35:25 -0400

    changes

---
+++ src/components/Background.jsx
@@ -0,0 +1,5 @@
+export const Background = () => {
+  return (
+    <div className="fixed inset-0  overflow-hidden pointer-events-none z-0"></div>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Background } from "../components/Background";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen  bg-[#0f1111] text-foreground overflow-x-hidden">
+      <Background />
+    </div>
+  );
+};
